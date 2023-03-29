aa6df8a1166744bbe81e1d524de9f02ebfd78aea
Kevin Salazar <kevinsalazarvillanueva@gmail.com>
2023-03-29 12:42:55 -0500

    upgrade

--- app/Http/Controllers/Admin/UsuarioController.php
+++ app/Http/Controllers/Admin/UsuarioController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Admin\TipoUsuario;
 use App\Models\Admin\Persona;
-use App\Models\Admin\OpcionMenu;
 use App\Models\Admin\Usuario;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
