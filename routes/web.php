9ec0eaf0ef48529145bc09bd8cd321efea1ad5ee
Kevin Salazar <kevinsalazarvillanueva@gmail.com>
2023-05-17 19:30:24 -0500

    upgrade

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Admin\TicketController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
