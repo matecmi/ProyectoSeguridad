f19f8702a2b7b85c2f45f77142958e01829ffe81
Kevin Salazar <kevinsalazarvillanueva@gmail.com>
2023-05-19 12:22:28 -0500

    upgrade

--- resources/views/admin/faq.blade.php
+++ resources/views/admin/faq.blade.php
@@ -105,7 +105,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
-<script src="{{ asset('datatables/datatables.js') }}"></script>
+<script src="{{ asset('DataTables/datatables.js') }}"></script>
 
 <script> 
 
