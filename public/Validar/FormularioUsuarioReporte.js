66283184a21b62c47cfce9b4151067a1071cbb48
Gastelo <agastelo@360salesolutions.com>
2023-05-25 10:42:44 -0500

    se agrega validaciones a los formularios

--- public/Validar/FormularioUsuarioReporte.js
+++ public/Validar/FormularioUsuarioReporte.js
@@ -7,7 +7,7 @@ const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+	telefono: /^\d{9}$/ // 7 a 14 numeros.
 }
 
 const campos = {
